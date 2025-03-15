61dfb886fd2be58a7d43bf012764b874306104e5
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-16 03:20:03 +0700

    feat: definisi custom errors

--- internal/application/task_service.go
+++ internal/application/task_service.go
@@ -1,1 +1,9 @@
 package application
+
+import "errors"
+
+var (
+	ErrTaskNotFound     = errors.New("tugas tidak ditemukan")
+	ErrInvalidStatus    = errors.New("status tidak valid")
+	ErrEmptyDescription = errors.New("deksripksi tidak boleh kosong")
+)
