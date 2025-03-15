fb7e57706fa2b0be66475b8670b8b89cfa9129b8
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-16 03:46:29 +0700

    feat: create method LoadTask()

--- infrastructure/storage.go
+++ infrastructure/storage.go
@@ -1,6 +1,11 @@
 package infrastructure
 
-import "github.com/andrMaulana/go-simple-task-tracker/internal/domain"
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/andrMaulana/go-simple-task-tracker/internal/domain"
+)
 
 type Storage interface {
 	LoadTasks() (domain.TaskList, error)
@@ -11,4 +16,15 @@ type JsonStorage struct{}
 
 func NewJsonStorage() *JsonStorage {
 	return &JsonStorage{}
+}
+
+func (s *JsonStorage) LoadTasks() (domain.TaskList, error) {
+	var taskList domain.TaskList
+	data, err := os.ReadFile("tasks.json")
+	if err != nil {
+		return taskList, err
+	}
+
+	json.Unmarshal(data, &taskList)
+	return taskList, nil
 }
