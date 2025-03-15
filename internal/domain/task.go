711067772e33e0fdb2ed5d4b04e93bee7b83c49f
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-16 03:16:26 +0700

    refactor: recode struct data to task.go

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -1,1 +1,15 @@
 package domain
+
+import "time"
+
+type Task struct {
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+type TaskList struct {
+	Tasks []Task `json:"tasks"`
+}
