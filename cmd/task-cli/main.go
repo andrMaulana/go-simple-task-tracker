39d037e7cb325a52b183ce6aacae4fffaed32291
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-16 04:29:05 +0700

    feat: load storage dan service di main

--- cmd/task-cli/main.go
+++ cmd/task-cli/main.go
@@ -1,4 +1,11 @@
 package main
 
+import (
+	"github.com/andrMaulana/go-simple-task-tracker/infrastructure"
+	"github.com/andrMaulana/go-simple-task-tracker/internal/application"
+)
+
 func main() {
+	storage := infrastructure.NewJsonStorage()
+	service := application.NewTaskService()
 }
