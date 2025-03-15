5002e8d9931b969308dac4dbc25b03c17f63fde6
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-16 03:42:42 +0700

    feat: create interface task storage

--- infrastructure/storage.go
+++ infrastructure/storage.go
@@ -1,1 +1,14 @@
 package infrastructure
+
+import "github.com/andrMaulana/go-simple-task-tracker/internal/domain"
+
+type Storage interface {
+	LoadTasks() (domain.TaskList, error)
+	SaveTasks(domain.TaskList) error
+}
+
+type JsonStorage struct{}
+
+func NewJsonStorage() *JsonStorage {
+	return &JsonStorage{}
+}
