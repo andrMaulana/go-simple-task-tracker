cb907e4c5893eb1e70aa8f1752ee974191f64aef
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-18 17:35:29 +0700

    Feat: update model data task untuk fitur priority

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -3,11 +3,13 @@ package domain
 import "time"
 
 type Task struct {
-	ID          int       `json:"id"`
-	Description string    `json:"description"`
-	Status      string    `json:"status"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	ID          int        `json:"id"`
+	Description string     `json:"description"`
+	Status      string     `json:"status"`
+	Priority    string     `json:"priority"`          // Akan diimplementasi di fase 3
+	DueDate     *time.Time `json:"dueDate,omitempty"` // Nullable
+	CreatedAt   time.Time  `json:"createdAt"`
+	UpdatedAt   time.Time  `json:"updatedAt"`
 }
 
 type TaskList struct {
